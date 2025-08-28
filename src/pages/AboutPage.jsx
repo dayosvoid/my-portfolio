7f9af0d1f2c2940ad16bba5661ff8d3fd4729e27
Adedayo <atimileyin911@gmail.com>
2025-08-28 16:33:33 +0100

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MoreAboutMe from '../component/MoreAboutMe'
 import Exp from '../component/Exp'
 import Connent from '../component/Connent'
 import AboutNav from '../component/AboutNav'
 
 const AboutPage = () => {
+  // to scroll to the top of rendering page
+  useEffect(() => {
+  window.scrollTo(0, 0);
+}, [])
   return (
     <div className='py-3'>
       <AboutNav/>
